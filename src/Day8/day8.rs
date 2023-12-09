cb1792afec35b34d2bc1c4763db39c35f9fecd55
Göllner Darius <da.goellner09@gmail.com>
2023-12-09 23:59:45 +0100

    Merged Day 8 Part 1 and 2 into 1 function

--- src/Day8/day8.rs
+++ src/Day8/day8.rs
@@ -3,79 +3,69 @@ pub mod day8 {
     use std::fs;
     use num_integer::lcm;
     use petgraph::graph::{DiGraph, NodeIndex};
-    use petgraph::prelude::EdgeRef;
+    use petgraph::visit::EdgeRef;
+
     use regex::Regex;
 
     pub fn solve() {
-        part1();
-        // part2();
-        part2_1();
+        part1_2();
     }
 
-    fn part1() {
+
+
+    fn part1_2() {
         let input = fs::read_to_string("src/Day8/input.txt").unwrap();
-        let lines = input.split("\n").collect::<Vec<&str>>();
-        let mut directions = lines[0].chars().collect::<Vec<char>>();
-        directions.pop();
-        let mut nodes: HashMap<String, String> = HashMap::new();
-
-        //Genrate map with the nodes
+        let lines: Vec<&str> = input.lines().collect();
+        let directions: Vec<char> = lines[0].chars().collect();
+        let mut aaa_index: usize = 0;
+        let mut start_points: Vec<NodeIndex> = Vec::new();
+        let mut node_string: HashMap<&str, NodeIndex> = HashMap::new();
+        let mut graph = DiGraph::new();
         for i in 2..lines.len() {
-            let start = lines[i].split("=").next().unwrap().trim().to_string();
-            let l_node = lines[i].split("(").last().unwrap().split(",").next().unwrap().trim().to_string();
-            let mut r_node = lines[i].split("(").last().unwrap().split(",").last().unwrap().trim().to_string();
-            r_node = (&r_node[0..3]).to_string();
+            let test = Regex::new(r"[a-zA-Z0-9]{3}").unwrap();
+            let nodevec: Vec<&str> = test.captures_iter(lines[i])
+                .map(|capture| capture.get(0).unwrap().as_str())
+                .take(3) // Take the first 3 matches
+                .collect();
 
-            nodes.insert(start.clone() + "R", r_node);
-            nodes.insert(start + "L", l_node);
-        }
-
-
-        let mut end = "AAA".to_string();
-        let mut count: usize = 0;
-        while end != "ZZZ" {
-            end = nodes.get((end + directions[(count % directions.len())].to_string().as_str()).as_str()).expect("{end}").to_string();
-            count += 1;
-        }
-        println!("Day 8 Part 1: {count}");
-    }
-
-    fn part2_1() {
-        let input = fs::read_to_string("src/Day8/input.txt").unwrap();
-        let lines = input.split("\n").collect::<Vec<&str>>();
-        let mut directions = lines[0].chars().collect::<Vec<char>>();
-        directions.pop();
-        let mut nodes: HashMap<String, String> = HashMap::new();
-        let mut starting_nodes: Vec<String> = Vec::new();
-
-        //Genrate map with the nodes
-        for i in 2..lines.len() {
-            let start = lines[i].split("=").next().unwrap().trim().to_string();
-            let l_node = lines[i].split("(").last().unwrap().split(",").next().unwrap().trim().to_string();
-            let mut r_node = lines[i].split("(").last().unwrap().split(",").last().unwrap().trim().to_string();
-            r_node = (&r_node[0..3]).to_string();
-
-            nodes.insert(start.clone() + "R", r_node);
-            nodes.insert(start.clone() + "L", l_node);
-            if (start.ends_with("A")) {
-                starting_nodes.push(start);
+            if !node_string.contains_key(nodevec[0].trim()) {
+                node_string.insert(nodevec[0].trim(), graph.add_node(nodevec[0].trim()));
+            }
+            if !node_string.contains_key(nodevec[1].trim()) {
+                node_string.insert(nodevec[1].trim(), graph.add_node(nodevec[1].trim()));
+            }
+            if !node_string.contains_key(nodevec[2].trim()) {
+                node_string.insert(nodevec[2].trim(), graph.add_node(nodevec[2].trim()));
+            }
+            graph.add_edge(*node_string.get(nodevec[0].trim()).unwrap(), *node_string.get(nodevec[1].trim()).unwrap(), ());
+            graph.add_edge(*node_string.get(nodevec[0].trim()).unwrap(), *node_string.get(nodevec[2].trim()).unwrap(), ());
+            if nodevec[0].ends_with('A') {
+                if nodevec[0].eq("AAA") {
+                    aaa_index = start_points.len();
+                }
+                start_points.push(*node_string.get(nodevec[0].trim()).unwrap());
             }
         }
 
         let mut end_node_count: Vec<u64> = Vec::new();
-        for starting_node in starting_nodes {
+        for start_point in &mut start_points {
             let mut count: usize = 0;
-            let mut end = starting_node.clone();
-            while !end.ends_with("Z") {
-                end = nodes.get((end + directions[(count % directions.len())].to_string().as_str()).as_str()).expect("{end}").to_string();
+            let mut end = start_point.clone();
+            while !graph[*start_point].ends_with("Z") {
+                if (directions[count % directions.len()] == 'R') {
+                    *start_point = graph.edges(*start_point).next().unwrap().target();
+                } else {
+                    *start_point = graph.edges(*start_point).last().unwrap().target();
+                }
                 count += 1;
             }
             end_node_count.push(count as u64);
         }
 
-        println!("Day 8 Part 2: {}",end_node_count.into_iter().fold(1, |acc, x| lcm(acc, x)));
+        println!("Day 8 Part 1: {}", end_node_count[(aaa_index) as usize]);
+        //19_631
+        println!("Day 8 Part 2: {}", end_node_count.into_iter().fold(1, |acc, x| lcm(acc, x)));
         //21_003_205_388_413
-
     }
 
 }
